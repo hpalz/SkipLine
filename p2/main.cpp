25ea9e1c9d90c54a9d2cb0ba9994725ea7433852
hpalz <hjpalz@gmail.com>
2024-07-12 18:53:20 -0500

    Problem 2

---
+++ p2/main.cpp
@@ -0,0 +1,48 @@
+// idea for jsoncpp came from here: https://open-source-parsers.github.io/jsoncpp-docs/doxygen/index.html
+#include "json/json.h"
+#include <fstream>
+#include <iostream>
+#include <set>
+using namespace std;
+void checkInSet(map<string, string> m, string fileName);
+
+int main() {
+  map<string, string> masterMap;
+  std::ifstream f("languageFiles/master_enum.json");
+  Json::Value root;
+  f >> root;
+  //iterates through each high level json object
+  for (auto a : root) {
+    auto enums = a["enums"];
+    string screenName = a["name"].asString();
+    //iterates through each screen's enum
+    for (auto b : enums) {
+      //Creating an entry in the map of {key: enumName, value: screenName}
+      masterMap.insert({b["name"].asString(),screenName});
+    }
+  }
+  checkInSet(masterMap,"languageFiles/lang_fr.json");
+  checkInSet(masterMap,"languageFiles/lang_de.json");
+  checkInSet(masterMap,"languageFiles/lang_en-us.json");
+  checkInSet(masterMap,"languageFiles/lang_es.json");
+}
+
+// puts new language file into set, and iterates through the master map to 
+// check if the language file has the same enums
+void checkInSet(map<string, string> m, string fileName){
+  set<string> subSet;
+  std::ifstream f(fileName);
+  Json::Value root;
+  f >> root;
+  for (auto a : root) {
+      string name = a["enum"].asString();
+      subSet.insert(name);
+  }
+
+  for (auto it = m.begin();
+     it != m.end(); ++it){
+       if(subSet.count(it->first)==0){
+         cout << it->first << "\t" << it->second << "\t" << "("<< fileName<< ")" << endl;
+       }
+     }
+}
